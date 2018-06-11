c8b67d69d2db3d3afccdb12f5c536a299c5940d7
Simon <Simon_Freelance@hotmail.com>
2018-06-10 20:38:46 -0400

    Gulpfile got more functionality from other packages and tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,45 @@
-const gulp = require('gulp'),
-less = require('gulp-less'),
-path = require('path'),
-browserSync = require('browser-sync').create();
+const gulp = require('gulp');
+const less = require('gulp-less');
+const plumber = require('gulp-plumber');
+const notify = require('gulp-notify');
+const browserSync = require('browser-sync');
+
+function customPlumber(errTitle) {
+    return plumber({
+        errorHandler: notify.onError({
+            // Customizing error title
+            title: errTitle || "Error running Gulp",
+            message: "Error: <%= error.message %>",
+        })
+    });
+}
 
 gulp.task('less', function () {
-  return gulp.src('./less/index.less')
-    .pipe(less({
-      paths: [ path.join(__dirname, 'less', 'includes') ]
-    }))
-    .pipe(gulp.dest('./css'));
+    return gulp.src('./less/index.less')
+        .pipe(customPlumber('Error Running Less'))
+        .pipe(less())
+        .pipe(gulp.dest('./css'))
+        .pipe(browserSync.reload({
+            stream: true,
+        }))
+});
+
+gulp.task('browserSync', function () {
+    browserSync({
+        server: {
+            baseDir: '.'
+        },
+    })
+});
+
+
+gulp.task('watch', ['browserSync', 'less'], function () {
+    gulp.watch('./less/**/*.less', ['less']);
+    gulp.watch('*.html').on('change', browserSync.reload);
+    gulp.watch('js/**/*.js').on('change', browserSync.reload);
+});
+
+
+gulp.task('default', ['watch'], function () {
+    console.log('Good to Go!');
 });
